2ff9b2370e6431a9703e0b5f1d2d17f92ea390de
Mariia Korzun <3151020m@gmail.com>
2020-11-17 02:27:19 +0200

    task 1 is done.

--- src/even-numbers.js
+++ src/even-numbers.js
@@ -1,6 +1,13 @@
 const evenNumbersInArray = (array) => {
-    // Your implementation
-    // Read README.md file, if you not understand what to do
+    if (!Array.isArray(array) || array.length === 0) {
+        return 'Passed argument is not an array or empty'
+    }
+    let divisor = 2
+    let newArray = array.filter(element => element % divisor === 0)
+    if (newArray.length === 0) {
+        return 'Passed array does not contain even numbers'
+    }
+    return newArray
 };
 
 module.exports = evenNumbersInArray;
